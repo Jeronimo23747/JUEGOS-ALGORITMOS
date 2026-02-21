462846dd867c0d1902506dc9242c48d890c3d73a
Jeronimo Hoyos <jhoyos_1280@unihumboldt.edu.co>
2026-02-21 15:23:11 -0500

    JUEGOS ALGORITMOS

---
+++ Juego.js
@@ -0,0 +1,7 @@
+let Juan = "lleva"
+let Samuel = "No lleva"
+let Martín = "No lleva"
+let Sara = "No lleva"
+if ("Juan" toca a "Samuel",) {console.log ("Samuel lleva")
+} else if ("Juan" toca a "Martín") {console.log ("Martín lleva")   
+} 
